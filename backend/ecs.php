96a6fd217761fa5d8601a0306cec7bf5458598df
Haymaker <haymaker@users.noreply.github.com>
2023-04-01 14:59:31 -0400

    chore(backend): ecs config

---
+++ backend/ecs.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Copyright (c) 2023 VWX Systems
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    // this way you add a single rule
+    $ecsConfig->rules([
+        NoUnusedImportsFixer::class,
+    ]);
+
+
+    // this way you can add sets - group of rules
+    $ecsConfig->sets([
+        // run and fix, one by one
+        // SetList::SPACES,
+        // SetList::ARRAY,
+        // SetList::DOCBLOCK,
+        // SetList::NAMESPACES,
+        // SetList::COMMENTS,
+        SetList::PSR_12,
+    ]);
+};
