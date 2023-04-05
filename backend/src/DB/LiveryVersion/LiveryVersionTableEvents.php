35fd6370979ff556dce22762ad5446a9dc1dedb6
Haymaker <haymaker@users.noreply.github.com>
2023-04-04 21:58:30 -0400

    feat(db): set created at timestamp on insert

--- backend/src/DB/LiveryVersion/LiveryVersionTableEvents.php
+++ backend/src/DB/LiveryVersion/LiveryVersionTableEvents.php
@@ -1,17 +1,31 @@
 <?php
+/*
+ * Copyright (c) 2023 VWX Systems
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 declare(strict_types=1);
 
 namespace LiveryManager\DB\LiveryVersion;
 
-use Atlas\Query\Delete;
-use Atlas\Query\Insert;
-use Atlas\Query\Select;
-use Atlas\Query\Update;
 use Atlas\Table\Row;
 use Atlas\Table\Table;
 use Atlas\Table\TableEvents;
-use PDOStatement;
+
+use function date;
 
 class LiveryVersionTableEvents extends TableEvents
 {
+    public function beforeInsertRow(Table $table, Row $row): ?array
+    {
+        $row->created_at = date('Y-m-d H:i:s');
+        return null;
+    }
 }
