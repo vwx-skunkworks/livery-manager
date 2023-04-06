83575dec508052365efb4d5ec08ba6446300dae3
Haymaker <haymaker@users.noreply.github.com>
2023-04-06 03:12:46 -0400

    feat(backend): identifier trait

---
+++ backend/src/Domain/IdentifierTrait.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (c) 2023 VWX Systems
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+declare(strict_types=1);
+
+namespace LiveryManager\Domain;
+
+use Odan\Tsid\Tsid;
+
+trait IdentifierTrait
+{
+    public readonly Tsid $uid;
+
+    public function getId(): string
+    {
+        return $this->uid->toString();
+    }
+
+    public function getUid(): int
+    {
+        return $this->uid->toInt();
+    }
+
+}
